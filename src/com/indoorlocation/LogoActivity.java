afefdf646c27640a497654d7e18e1c5cd4934ce1
16ren <c_h_e_n_l_e_i@126.com>
2014-10-30 09:25:00 +0800

    first commit
    Change-Id: I9ecc1bfa73a2e7538b2ef1936b38183f6c800a05
    Signed-off-by: 16ren <c_h_e_n_l_e_i@126.com>

---
+++ src/com/indoorlocation/LogoActivity.java
@@ -0,0 +1,65 @@
+package com.indoorlocation;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.content.Intent;
+public class LogoActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_logo);
+		ImageView imageLogo = (ImageView)findViewById(R.id.img_logo);
+		Animation animation = AnimationUtils.loadAnimation(this, R.anim.logoanimation);
+		imageLogo.setAnimation(animation);
+		animation.setAnimationListener(new AnimationListener() {
+			
+			@Override
+			public void onAnimationStart(Animation animation) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onAnimationRepeat(Animation animation) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onAnimationEnd(Animation animation) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(LogoActivity.this,StartActivity.class);
+				startActivity(intent);
+				LogoActivity.this.finish();
+			}
+		});
+		animation.startNow();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.logo, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
